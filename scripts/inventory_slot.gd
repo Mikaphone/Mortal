fb5f8c782078c18bd30e13003f158d894e1fd720
Mikaphone <123015485+Mikaphone@users.noreply.github.com>
2025-02-02 12:08:48 -0500

    Update inventory_slot.gd

--- scripts/inventory_slot.gd
+++ scripts/inventory_slot.gd
@@ -1,6 +1,13 @@
 extends Button
 
+@export var stored_item : Items
+
 func display(item:Items):
+	setitem(item)
 	self.icon = item.texture
 	self.text = item.name
 	$AmountLabel.text = item.item_amount
+
+func setitem(item: Items):
+	stored_item = item
+	
